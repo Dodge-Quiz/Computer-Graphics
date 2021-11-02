c0e4a28492de569266e1cb644d05d15fb37eaef1
Khouw Devin Nathanael <khouwdevin@gmail.com>
2021-11-02 11:08:41 +0700

    add all three js library

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import * as THREE from './three.js/build/three.module.js'
+import * as OrbitalControls from './three.js/examples/js/controls/OrbitControls.js'
 
 var scene, camera, renderer
+
+const Ground = () =>{
+    let object_geometry = new THREE.PlaneGeometry(100, 100)
+    let texture = new THREE.TextureLoader().load('./textures/grass.jpg')
+    texture.wrapT = THREE.RepeatWrapping
+    texture.wrapS = THREE.RepeatWrapping
+    texture.repeat.set(10, 10)
+    let material = new THREE.MeshBasicMaterial({
+        map : texture,
+        side : THREE.DoubleSide
+    })
+    let object = new THREE.Mesh(object_geometry, material)
+    object.receiveShadow = true
+    object.rotation.set(-Math.PI / 3, 0, 0)
+    return object
+}
 
 var doInit = () => {
     scene = new THREE.Scene()
@@ -11,16 +28,20 @@ var doInit = () => {
     const ASPECT =  WIDTH / HEIGHT
 
     camera = new THREE.PerspectiveCamera(FOV, ASPECT)  
-    camera.position.set(0, 0, 10)
+    camera.position.set(0, 0, 100)
     camera.lookAt(0, 0, 0)
     
     renderer = new THREE.WebGLRenderer({antialias: true})
     renderer.setSize(WIDTH, HEIGHT)
-    renderer.setClearColor(0x303030)
+    renderer.setClearColor(0xFFFFFF)
 
     document.body.appendChild(renderer.domElement)
 
-    /*Code Here*/
+    let controller = new OrbitalControls.PointerLockControls(camera, renderer.domElement)
+    let clock = new THREE.Clock()
+    controller.lock()
+
+    scene.add(Ground())
 }
 
 var doRender = () => {
