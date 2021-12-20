766a702628b528c28ccd97894c6b38632e67a11c
Khouw Devin Nathanael <61622468+khouwdevin@users.noreply.github.com>
2021-12-20 08:38:29 +0700

    Update skybox.js

--- components/skybox.js
+++ components/skybox.js
@@ -5,27 +5,27 @@ export const SkyBox = () =>{
     const geometry = new THREE.BoxGeometry(500, 500, 500)
     const material = [
         new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load('../textures/daylight_box_right.jpg')
+            map: new THREE.TextureLoader().load('./textures/daylight_box_right.jpg')
             , side: THREE.BackSide
         }),
         new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load('../textures/daylight_box_left.jpg')
+            map: new THREE.TextureLoader().load('./textures/daylight_box_left.jpg')
             , side: THREE.BackSide
         }),
         new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load('../textures/daylight_box_top.jpg')
+            map: new THREE.TextureLoader().load('./textures/daylight_box_top.jpg')
             , side: THREE.BackSide
         }),
         new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load('../textures/daylight_box_bottom.jpg')
+            map: new THREE.TextureLoader().load('./textures/daylight_box_bottom.jpg')
             , side: THREE.BackSide
         }),
         new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load('../textures/daylight_box_front.jpg')
+            map: new THREE.TextureLoader().load('./textures/daylight_box_front.jpg')
             , side: THREE.BackSide
         }),
         new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load('../textures/daylight_box_back.jpg')
+            map: new THREE.TextureLoader().load('./textures/daylight_box_back.jpg')
             , side: THREE.BackSide
         }),
     ]
